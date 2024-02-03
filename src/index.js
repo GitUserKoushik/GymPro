94a2d99020094d0159c18f4bd52660626a460119
GitUserKoushik <kouhsikchakraborty400@gmail.com>
2024-02-03 21:50:56 +0530

    repair2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+// import "react-toastify/dist/ReactToastify.css";
+
 
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
